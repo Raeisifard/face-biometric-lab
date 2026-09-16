bae9375f4a5e6a1504fc289737f68fd738b42a9a
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-16 13:33:58 +0330

    Fix Method 1 OpenCV native loading regression

--- face-biometric-service/src/main/java/com/isc/facebiometricservice/config/Method1Config.java
+++ face-biometric-service/src/main/java/com/isc/facebiometricservice/config/Method1Config.java
@@ -1,11 +1,9 @@
 package com.isc.facebiometricservice.config;
 
-import org.opencv.core.Core;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(Method1Properties.class)
 public class Method1Config {
- static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 }
