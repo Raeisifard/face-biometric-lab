6522a6f2e86964be2659567c0e49c8954af88fd3
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-16 13:37:37 +0330

    Fix bundled OpenCV native initialization for Method 1

--- face-biometric-service/src/main/java/com/isc/facebiometricservice/config/Method1Config.java
+++ face-biometric-service/src/main/java/com/isc/facebiometricservice/config/Method1Config.java
@@ -1,9 +1,13 @@
 package com.isc.facebiometricservice.config;
 
+import nu.pattern.OpenCV;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(Method1Properties.class)
 public class Method1Config {
+    static {
+        OpenCV.loadLocally();
+    }
 }
