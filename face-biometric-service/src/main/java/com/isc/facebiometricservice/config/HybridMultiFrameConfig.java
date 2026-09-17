11d9b3c4096138a8929245f4526b21bd7ca3cca1
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-17 18:52:41 +0330

    Register hybrid multi-frame configuration

---
+++ face-biometric-service/src/main/java/com/isc/facebiometricservice/config/HybridMultiFrameConfig.java
@@ -0,0 +1,9 @@
+package com.isc.facebiometricservice.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HybridMultiFrameProperties.class)
+public class HybridMultiFrameConfig {
+}
